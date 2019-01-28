fa8ba243f2f93862ca24b8483624cc96c30f76f8
kaihe1 <42453853+kaihe1@users.noreply.github.com>
2019-01-28 13:18:57 -1000

    Add files via upload

--- back.js
+++ back.js
@@ -4,30 +4,16 @@
       xmlhttp.onreadstatechange = function(){
         if(this.readyState == 4 && this.status == 200){
         var mydata = JSON.parse(this.responseText);
-        var steak = mydata.food[0].price;
-        price.push(steak);
+        var steakN1 = mydata.food[0].price;
+        price.push(steakN1);
         var gSteak = mydata.food[0].name;
         meat.push(gSteak);
-        document.getElementById('Gsteak').innerHTML =mydata.food[0].name;
+        document.getElementById('Gsteak').innerHTML = mydata.food[0].name;
         localStorage.setItem(mydata.food[0].name);
-        
-    /*    var salmon = mydata.food[1].price;
-        var grilledSalmon = mydata.food[1].name;
-
-        var chicken = mydata.food[2].price;
-        var grilledChicken = mydata.food[2].name;
-
-        var shrimp = mydata.food[3].price;
-        var grilledShrimp = mydata.food[3].name;
-
-        var pork = mydata.food[4].price;
-        var grilledPork = mydata.food[4].name;
-
-        var goat = mydata.food[5].price;
-        var grilledGoat = mydata.food[5].name;
-      */
         }
       };
+      mlhttp.open("GET","sample.json",true);
+      xmlhttp.send();
 /*   function purchase(){
   var x = document.getElementById('user').value;
   document.getElementById('user').value = "";
