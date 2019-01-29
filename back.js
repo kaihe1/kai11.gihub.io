d3a5b4195f140f6a0bba5ec8c548beb15b160263
kaihe1 <42453853+kaihe1@users.noreply.github.com>
2019-01-29 11:53:58 -1000

    Add files via upload

--- back.js
+++ back.js
@@ -1,6 +1,5 @@
 var meats=[];
 var prices= [];
-var tax = 1.04712;
 
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function(){
@@ -18,19 +17,11 @@ xmlhttp.open("GET" , "sample.json", true);
 xmlhttp.send
 
 function purchase1(){
-  var beforeTaxes = prices[0];
-  var result1 = document.getElementById('button1').value;
-  document.getElementById('button1').value = "";
-  var pT = result1 * beforeTaxes;
-  var taxP = pT * tax;
-  var fP = taxP.toFixed(2);
-  if(Result1 >0){
-  window.alert('Your food has been added,Scroll down to finish your purchase');
-  var newli = document.createElement('li');
-  var newText = document.createTextNode(result1 + " " + meats[0] + " $" + fP);
- newli.appendChild(newText);
- var olTag = document.getElementsByTagName('ol')[0];
- olTag.appendChild(newli);
-  }
-  localStorage.setItem("foodS1", newli);
+        var x = document.getElementById('user1').value;
+        document.getElementById('user1').value = "";
+        var li = document.createElement('li');
+        var newText = document.createTextNode(x + meats[0]);
+        li.appendChild(newText);
+        var olTag = document.getElementsByTagName('ol')[0];
+        olTag.appendChild(li);
 }
