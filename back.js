9a35a8f8e85a68d9804892f2b69c895c817d8d93
kaihe1 <42453853+kaihe1@users.noreply.github.com>
2019-01-29 12:27:58 -1000

    Add files via upload

--- back.js
+++ back.js
@@ -1,6 +1,6 @@
 var meats=[];
 var prices= [];
-
+var de =[];
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function(){
   if(this.readyState == 4 && this.status == 200){
@@ -9,12 +9,15 @@ xmlhttp.onreadystatechange = function(){
     meats.push(gsteak);
     var psteak = mydata.meats[0].price;
     prices.push(psteak);
+    var dsteak = mydata.meats[0].descriptions;
+    de.push(dsteak);
     document.getElementById('Gsteak').innerHTML = mydata.meats[0].name;
-    document.getElementById('p1').innerHTML = "$" + mydata.meats[0].price;
+    document.getElementById('p1').innerHTML =  mydata.meats[0].price;
+    document.getElementById('d1').innerHTML =  mydata.meats[0].descriptions;
 }
 };
 xmlhttp.open("GET" , "sample.json", true);
-xmlhttp.send
+xmlhttp.send;
 
 function purchase1(){
         var x = document.getElementById('user1').value;
