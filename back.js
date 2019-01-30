873acae3cfbe59939912d0af3a33d23ee608c11c
kaihe1 <42453853+kaihe1@users.noreply.github.com>
2019-01-30 12:16:36 -1000

    Add files via upload

--- back.js
+++ back.js
@@ -86,7 +86,7 @@ function purchase1(){
         var subtotal = (x * 22).toFixed(2);
         var total = ((x * 22) * 1.04712).toFixed(2);
         totalp.push(total);
-        localStorage.setItem("total", total);
+        localStorage.setItem("tota1", total);
         var li = document.createElement('li');
         var newText = document.createTextNode(x + " Order of " +" "+ meats[0] + " " + " sub total is $" + subtotal + " and total after tax  is $" + total);
         li.appendChild(newText);
@@ -100,6 +100,7 @@ function purchase2(){
   var subtotal = (x * 17).toFixed(2);
   var total = ((x * 17) * 1.04712).toFixed(2);
   totalp.push(total);
+  localStorage.setItem("tota2", total);
   var li = document.createElement('li');
   var newText = document.createTextNode(x + " Order of " +" "+ meats[1] + " " + " sub total is $" + subtotal + " and total after tax  is $" + total);
   li.appendChild(newText);
@@ -114,6 +115,7 @@ function purchase3(){
   var subtotal = (x * 14).toFixed(2);
   var total = ((x * 14) * 1.04712).toFixed(2);
   totalp.push(total);
+  localStorage.setItem("tota3", total);
   var li = document.createElement('li');
   var newText = document.createTextNode(x + " Order of " +" "+ meats[2] + " " + " sub total is $" + subtotal + " and total after tax  is $" + total);
   li.appendChild(newText);
@@ -127,6 +129,7 @@ function purchase4(){
   var subtotal = (x * 20).toFixed(2);
   var total = ((x * 20) * 1.04712).toFixed(2);
   totalp.push(total);
+  localStorage.setItem("tota4", total);
   var li = document.createElement('li');
   var newText = document.createTextNode(x + " Order of " +" "+ meats[3] + " " + " sub total is $" + subtotal + " and total after tax  is $" + total);
   li.appendChild(newText);
@@ -140,6 +143,7 @@ function purchase5(){
   var subtotal = (x * 18).toFixed(2);
   var total = ((x * 18) * 1.04712).toFixed(2);
   totalp.push(total);
+  localStorage.setItem("tota5", total);
   var li = document.createElement('li');
   var newText = document.createTextNode(x + " Order of " +" "+ meats[4] + " " + " sub total is $" + subtotal + " and total after tax  is $" + total);
   li.appendChild(newText);
@@ -153,6 +157,7 @@ function purchase6(){
   var subtotal = (x * 21).toFixed(2);
   var total = ((x * 21) * 1.04712).toFixed(2);
   totalp.push(total);
+  localStorage.setItem("tota6", total);
   var li = document.createElement('li');
   var newText = document.createTextNode(x + " Order of " +" "+ meats[5] + " " + " sub total is $" + subtotal + " and total after tax  is $" + total);
   li.appendChild(newText);
@@ -160,7 +165,6 @@ function purchase6(){
   olTag.appendChild(li);
 }
 
-function sumbit(){
-  localStorage.
-  
-  }
+function result(){
+  location.href="index2.html";
+}
